3749e023be8e5d04c6372d03d505dad2b56dc1b5
Vaisakh K <vaisakh.k@amagi.com>
2024-12-21 21:38:05 +0530

    message

--- app/server.go
+++ app/server.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"net/http"
 	"os"
 )
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		msg := "HTTP/1.1 200 OK\r\n\r\n"
+		if r.URL.Path != "/" {
+			msg = "HTTP/1.1 404 Not Found\r\n\r\n"
+		}
+		w.Write([]byte(msg))
+	})
+
+	err := http.ListenAndServe("0.0.0.0:4221", nil)
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
