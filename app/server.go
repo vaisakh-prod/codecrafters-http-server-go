f070bae79fca47d269fbcd5fd39d6f51e788a483
Vaisakh K <vaisakh.k@amagi.com>
2024-12-22 04:01:49 +0530

    .

--- app/server.go
+++ app/server.go
@@ -9,24 +9,38 @@ import (
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
-	msg := "HTTP/1.1 404 Not Found\r\n\r\n"
 	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		handleConnection(conn)
 	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	msg := "HTTP/1.1 404 Not Found\r\n\r\n"
 	req := make([]byte, 1024)
-	conn.Read(req)
-	fmt.Print(string(req))
 	lines := strings.Split(string(req), "\r\n")
+	conn.Read(req)
+
+	fmt.Print(string(req))
+
+	defer conn.Close()
+
 	var path string
 	items := []string{}
+
 	for _, line := range lines {
 		if strings.Contains(line, "GET") {
 			path = strings.Split(line, " ")[1]
@@ -34,6 +48,7 @@ func main() {
 			items = strings.Split(line, ": ")
 		}
 	}
+
 	if path == "/" {
 		msg = "HTTP/1.1 200 OK\r\n\r\n"
 	} else if strings.HasPrefix(path, "/echo/") {
@@ -42,5 +57,6 @@ func main() {
 	} else if strings.HasPrefix(path, "/user-agent") && len(items) > 0 {
 		msg = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%v", len(items[1]), items[1])
 	}
+
 	conn.Write([]byte(msg))
 }
