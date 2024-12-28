39b4e00c0e3e3eb51f2a2cf21e30400ff824c218
Vaisakh K <vaisakh.k@amagi.com>
2024-12-28 21:09:29 +0530

    added content-encoding

--- app/server.go
+++ app/server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"net"
@@ -34,74 +37,121 @@ func main() {
 
 }
 
-func handleConnection(conn net.Conn, Directory *string) {
-	msg := "HTTP/1.1 404 Not Found\r\n\r\n"
-	req := make([]byte, 1024)
-
-	n, _ := conn.Read(req)
-
-	req = req[:n]
-	lines := strings.Split(string(req), "\r\n")
+func handleConnection(conn net.Conn, directory *string) {
 	defer conn.Close()
 
-	var path string
-	items := []string{}
+	req := make([]byte, 1024)
+	n, err := conn.Read(req)
+	if err != nil {
+		sendResponse(conn, "HTTP/1.1 500 Internal Server Error\r\n\r\n")
+		return
+	}
+	req = req[:n]
+	lines := strings.Split(string(req), "\r\n")
 
-	fmt.Println(lines, len(req))
+	path, userAgent, encodingFormat := parseRequest(lines)
+	if path == "" {
+		sendResponse(conn, "HTTP/1.1 400 Bad Request\r\n\r\n")
+		return
+	}
+
+	switch {
+	case strings.HasPrefix(path, "/echo/"):
+		handleEcho(conn, path, encodingFormat)
+	case strings.HasPrefix(path, "/user-agent"):
+		handleUserAgent(conn, userAgent)
+	case strings.HasPrefix(path, "/files/"):
+		handleFiles(conn, directory, path, lines)
+	default:
+		handleDefault(conn, path)
+	}
+}
+
+func parseRequest(lines []string) (string, string, string) {
+	var path, userAgent, encodingFormat string
 	for _, line := range lines {
-		if strings.Contains(line, "GET") || strings.Contains(line, "POST") {
-			path = strings.Split(line, " ")[1]
-
-		} else if strings.Contains(line, "User-Agent") {
-			items = strings.Split(line, ": ")
+		if strings.HasPrefix(line, "GET") || strings.HasPrefix(line, "POST") {
+			path = strings.Fields(line)[1]
+		} else if strings.HasPrefix(line, "User-Agent") {
+			userAgent = strings.SplitN(line, ": ", 2)[1]
+		} else if strings.HasPrefix(line, "Accept-Encoding") {
+			encodingFormat = strings.SplitN(line, ": ", 2)[1]
 		}
 	}
-	fmt.Println(path)
+	return path, userAgent, encodingFormat
+}
 
-	if strings.Contains(lines[0], "GET") {
-		fmt.Println(path)
-		if path == "/" {
-			msg = "HTTP/1.1 200 OK\r\n\r\n"
-		} else if strings.HasPrefix(path, "/echo/") {
-			keyword := strings.Split(path, "/echo/")[1]
-			msg = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%v", len(keyword), keyword)
-		} else if strings.HasPrefix(path, "/user-agent") && len(items) > 0 {
-			msg = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%v", len(items[1]), items[1])
-		} else if strings.HasPrefix(path, "/files/") {
-			fileName := strings.Split(path, "/files/")[1]
-			if *Directory != "" && fileName != "" {
-				absolutePath := filepath.Join(*Directory, fileName)
+func gzipAndEncode(data string) (string, error) {
+	var b bytes.Buffer
+	gz := gzip.NewWriter(&b)
+	_, err := gz.Write([]byte(data))
+	if err != nil {
+		return "", err
+	}
+	if err := gz.Close(); err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(b.Bytes()), nil
+}
 
-				fileData, err := os.ReadFile(absolutePath)
-				//fmt.Println(absolutePath, fileData)
-				if err != nil {
-					fmt.Print(err)
-				}
-				if err == nil {
-					file := string(fileData)
-					msg = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%v", len(file), file)
-				}
-			} else {
-				panic("Directory or fileName is None")
-			}
+func handleEcho(conn net.Conn, path, encodingFormat string) {
+	keyword := strings.TrimPrefix(path, "/echo/")
+	msg := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%v", len(keyword), keyword)
+	if encodingFormat == "gzip" {
+		encodedKeyword, err := gzipAndEncode(keyword)
+		if err != nil {
+			fmt.Printf("Failed to gzip and encode: %v\n", err)
+			sendResponse(conn, "HTTP/1.1 500 Internal Server Error\r\n\r\n")
+			return
 		}
-	} else if strings.Contains(lines[0], "POST") {
+		msg = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: %d\r\n\r\n%v", len(encodedKeyword), encodedKeyword)
+	}
+	sendResponse(conn, msg)
+}
+
+func handleUserAgent(conn net.Conn, userAgent string) {
+	msg := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%v", len(userAgent), userAgent)
+	sendResponse(conn, msg)
+}
+
+func handleFiles(conn net.Conn, directory *string, path string, lines []string) {
+	fileName := strings.TrimPrefix(path, "/files/")
+	if *directory == "" || fileName == "" {
+		sendResponse(conn, "HTTP/1.1 400 Bad Request\r\n\r\n")
+		return
+	}
+	absolutePath := filepath.Join(*directory, fileName)
+
+	if strings.HasPrefix(lines[0], "GET") {
+		fileData, err := os.ReadFile(absolutePath)
+		if err != nil {
+			sendResponse(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+			return
+		}
+		msg := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%v", len(fileData), string(fileData))
+		sendResponse(conn, msg)
+	} else if strings.HasPrefix(lines[0], "POST") {
 		data := []byte(lines[len(lines)-1])
-		fmt.Println(len(data))
-		if strings.HasPrefix(path, "/files/") {
-			fileName := strings.Split(path, "/files/")[1]
-			if *Directory != "" && fileName != "" {
-				absolutePath := filepath.Join(*Directory, fileName)
-				err := os.WriteFile(absolutePath, data, 0644)
-				if err != nil {
-					fmt.Print(err)
-				} else {
-					msg = "HTTP/1.1 201 Created\r\n\r\n"
-				}
-			} else {
-				panic("Directory or fileName is None")
-			}
+		err := os.WriteFile(absolutePath, data, 0644)
+		if err != nil {
+			sendResponse(conn, "HTTP/1.1 500 Internal Server Error\r\n\r\n")
+			return
 		}
+		sendResponse(conn, "HTTP/1.1 201 Created\r\n\r\n")
 	}
-	conn.Write([]byte(msg))
+}
+
+func handleDefault(conn net.Conn, path string) {
+	if path == "/" {
+		sendResponse(conn, "HTTP/1.1 200 OK\r\n\r\n")
+	} else {
+		sendResponse(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+}
+
+func sendResponse(conn net.Conn, msg string) {
+	_, err := conn.Write([]byte(msg))
+	if err != nil {
+		fmt.Printf("Failed to send response: %v\n", err)
+	}
 }
